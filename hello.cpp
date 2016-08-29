5b28a0be88cb2d8f416c605de2f2882e15183bf3
zwj5582 <zwj5582@gmail.com>
2016-08-29 01:03:23 -0700

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: zhongwenjie
+	> Mail: zwj5582@gmail.com 
+	> Created Time: Mon 29 Aug 2016 01:00:46 AM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"hello"<<endl;
+}
